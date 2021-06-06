9f371435ac5176977e708d5f3e2c52ca88cf3131
Xinyi Zhao <huanhuan@Xinyis-MacBook-Pro.local>
2021-06-06 15:49:52 -0400

    format files

--- src/app/card-details/card-details.component.ts
+++ src/app/card-details/card-details.component.ts
@@ -1,20 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Card } from '../classes/card';
+import { Component, Input, OnInit } from "@angular/core";
+import { Card } from "../classes/card";
 
 @Component({
-  selector: 'app-card-details',
-  templateUrl: './card-details.component.html',
-  styleUrls: ['./card-details.component.scss']
+  selector: "app-card-details",
+  templateUrl: "./card-details.component.html",
+  styleUrls: ["./card-details.component.scss"],
 })
 export class CardDetailsComponent implements OnInit {
-
   @Input() card!: Card;
   @Input() i!: number;
-  myId = 'card-details';
-  
-  constructor() { }
+  myId = "card-details";
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
