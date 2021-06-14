8a40452d61d4461ea67989456a45c3ef57841ca2
Xinyi Zhao <huanhuan@Xinyis-MacBook-Pro.local>
2021-06-14 14:21:28 -0400

    Auto-generated commit [ci skip]

--- src/app/classes/cards-response.ts
+++
@@ -1,7 +0,0 @@
-import { Card } from "./card";
-
-export interface CardsResponse {
-    cards: Card[];
-    _pageSize: number;
-    _totalCount: number;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  cardServiceURL: 'https://api.elderscrollslegends.io/v1/cards'
-};
